d95ea9b9acacb487d8c54cf5b92fd1985958f2ce
Eore <eore26@gmail.com>
2020-10-02 07:51:44 +0700

    New algorithm

--- geofence.go
+++ geofence.go
@@ -1,7 +1,11 @@
 package geofence
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
+type Point [2]*big.Float
 type State int
 
 const (
@@ -9,8 +13,7 @@ const (
 	Inside  State = 1
 )
 
-func GeoFenceCheck(p point, polygon []point) (State, error) {
-	count := 0
+func GeoFenceCheck(p Point, polygon []Point) (State, error) {
 	l := len(polygon)
 	if !isPointSame(polygon[0], polygon[l-1]) {
 		return Outside, fmt.Errorf("Polygon not closing")
@@ -18,22 +21,33 @@ func GeoFenceCheck(p point, polygon []point) (State, error) {
 	if l < 3 {
 		return Outside, fmt.Errorf("Is not polygon")
 	}
-	startPoint := polygon[0]
-	for i := 0; i < l-3; i++ {
-		if pointInTriangle(p, triangle{
-			vector{startPoint, polygon[i+1]},
-			vector{polygon[i+1], polygon[i+2]},
-			vector{polygon[i+2], startPoint},
-		}) {
-			count++
+	state := false
+	x, y := p[0], p[1]
+	for i := 0; i < l-1; i++ {
+		x1, y1 := polygon[i][0], polygon[i][1]
+		x2, y2 := polygon[i+1][0], polygon[i+1][1]
+		if y1.Cmp(y) != y2.Cmp(y) &&
+			x.Cmp(
+				new(big.Float).Add(
+					x1,
+					new(big.Float).Mul(
+						new(big.Float).Sub(x2, x1),
+						new(big.Float).Quo(
+							new(big.Float).Sub(y, y1),
+							new(big.Float).Sub(y2, y1),
+						),
+					),
+				),
+			) == -1 {
+			state = !state
 		}
 	}
-	if count == 0 || count%2 == 0 {
-		return Outside, nil
+	if state {
+		return Inside, nil
 	}
-	return Inside, nil
+	return Outside, nil
 }
 
-func isPointSame(p1, p2 point) bool {
+func isPointSame(p1, p2 Point) bool {
 	return p1[0].Cmp(p2[0]) == 0 && p1[1].Cmp(p2[1]) == 0
 }
